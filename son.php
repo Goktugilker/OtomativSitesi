d4b8296bcb54015fa4c3964f2be2485b0bc3a6fa
Göktuğ İlker <240928036@ogr.comu.edu.tr>
2025-03-24 15:08:49 +0300

    son.php

--- son.php
+++ son.php
@@ -2,10 +2,10 @@
             <div class="row footerrow ">
                 <div class="col-lg-3 footercol">
                     <br>
-                    <a href="index.php" class="text-light">Ana Sayfa</a><br>
-                    <a href="sayfalar/hakkımızda.php" class="text-light">Hakkımızda</a><br>
-                    <a href="sayfalar/galeri.php" class="text-light">Galeri</a><br>
-                    <a href="/sayfalar/iletisim.php" class="text-light">İletişim</a>
+                    <a href="/OtomativSitesi/index.php" class="text-light">Ana Sayfa</a><br>
+                    <a href="/OtomativSitesi/sayfalar/hakkımızda.php" class="text-light">Hakkımızda</a><br>
+                    <a href="/OtomativSitesi/sayfalar/galeri.php" class="text-light">Galeri</a><br>
+                    <a href="/OtomativSitesi/sayfalar/iletisim.php" class="text-light">İletişim</a>
                 </div>
                 <div class="col-lg-6 footercol2 text-center">
                     <br><p>Tüm Hakları Saklıdır<br>Göktuğ İlker®</p><br>
@@ -13,9 +13,9 @@
                 <div class="col-lg-3">
                     <div class="sm">
                         <p>Bizi sosyal medyadan takip edin.</p>
-                        <div class="smlogo logo1"><a href="https://instagram.com"><img src="img/ig.png" alt=""></a></div>
-                        <div class="smlogo logo2"><a href="https://x.com"><img src="img/x.png" alt=""></a></div>
-                        <div class="smlogo logo3"><a href="https://facebook.com"><img src="img/facebook.png" alt=""></a></div>
+                        <div class="smlogo logo1"><a href="https://instagram.com"><img src="/OtomativSitesi/img/ig.png" alt=""></a></div>
+                        <div class="smlogo logo2"><a href="https://x.com"><img src="/OtomativSitesi/img/x.png" alt=""></a></div>
+                        <div class="smlogo logo3"><a href="https://facebook.com"><img src="/OtomativSitesi/img/facebook.png" alt=""></a></div>
                     </div>
                 </div>
             </div>
